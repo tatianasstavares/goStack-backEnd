a9c50f2e5a51eaa17ac7911902634aa620ab1072
Tatiana <tatianasstavares@gmail.com>
2021-04-15 10:27:21 +0100

    Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+const { response } = require("express");
 const express = require("express");
 
 const app = express();
 
 app.get("/projects", (req, res) => {
-  return res.json({ message: "Hello Tati" });
+  return res.json(["projeto1", "projeto2"]);
+});
+
+app.post("/projects", (req, res) => {
+  return res.json(["projeto1", "projeto2", "projeto3"]);
+});
+
+app.put("/projects/:id", (req, res) => {
+  return res.json(["projeto4", "projeto2", "projeto3"]);
+});
+
+app.delete("/projects/:id", (req, res) => {
+  return res.json(["projeto2", "projeto3"]);
 });
 
 app.listen(3333, () => {
