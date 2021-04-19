55178aacf094f94bc4927d258c57f11c5ff2c129
Tatiana <tatianasstavares@gmail.com>
2021-04-19 11:31:03 +0100

    Tipos de parâmetros

--- src/index.js
+++ src/index.js
@@ -3,15 +3,25 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/projects", (req, res) => {
-  return res.json(["projeto1", "projeto2"]);
+  const query = req.query;
+
+  console.log(query);
+
+  return res.json(["projeto 1", "Projeto 2"]);
 });
 
 app.post("/projects", (req, res) => {
-  return res.json(["projeto1", "projeto2", "projeto3"]);
+  const { title, owner } = req.body;
+  console.log(title, owner);
+  return res.json(["p1,p2,p3"]);
 });
 
 app.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
   return res.json(["projeto4", "projeto2", "projeto3"]);
 });
 
